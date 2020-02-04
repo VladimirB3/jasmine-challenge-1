8034efde7954709754f8d1737f8958eed646bf02
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-04 11:00:52 +0000

    Challenge test writeup started

---
+++ jasmine-tests/spec/ageSpec.js
@@ -0,0 +1,8 @@
+describe("whatCanIDrink", function() {
+
+    describe("Age input", function(){
+        it("should return >0", function() {
+            expect(whatCanIDrink).toBe(0);
+        });
+    });
+});
